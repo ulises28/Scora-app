40f9aa1e5bf3d8f5a2befe7b4782e1e557c6f5d7
Ulises Saavedra <ulises_2692@hotmail.com>
2026-03-01 00:13:25 -0600

    fixing vite server

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 export const STRAVA_CONFIG = {
     CLIENT_ID: '203757',
     CLIENT_SECRET: 'b5763c11e86387a3f6fae3331b6f541825356eff',
-    REDIRECT_URI: 'http://192.168.51.250:5500'
+    REDIRECT_URI: window.location.origin
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     server: {
+        host: true,
         port: 5500, // Change this to whatever port Strava expects!
         strictPort: true
     },
