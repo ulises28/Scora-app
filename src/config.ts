04ac4881c79f630b8f756b3f1418cdb6c69ac13a
Ulises Saavedra <ulises_2692@hotmail.com>
2026-03-03 16:28:10 -0600

    chore: clean up unneeded files and ignore config secrets

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// js/config.js
-export const STRAVA_CONFIG = {
-    CLIENT_ID: '203757',
-    CLIENT_SECRET: 'b5763c11e86387a3f6fae3331b6f541825356eff',
-    REDIRECT_URI: window.location.origin
-};
