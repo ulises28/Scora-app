1c15a43d49da6d4656a43742064d17ce7276dd3f
Ulises Saavedra <ulises_2692@hotmail.com>
2026-03-05 17:24:33 -0600

    feat: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    base: '/Scora-app/',
     server: {
         host: true,
         port: 5500, // Change this to whatever port Strava expects!
